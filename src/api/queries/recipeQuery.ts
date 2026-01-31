2bf43a01069a6a0642233302617bae509525187a
mateuszm <mateuszm@pop-os.localdomain>
2026-01-31 03:18:30 +0100

    feat: check rules - add hardcoded api key

--- src/api/queries/recipeQuery.ts
+++ src/api/queries/recipeQuery.ts
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchRecipes } from "../services";
 
+const apiKey = "AIzaSyA1234567890";
+
 export const useGetChickenRecipes = () =>
   useQuery({
     queryKey: ["recipes", "chicken"],
