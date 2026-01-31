78873adb79371a9abd1ec5402aa3547dcfbfec8c
mateuszm <mateuszm@pop-os.localdomain>
2026-01-31 16:27:35 +0100

    feat: add new rules

--- src/api/queries/recipeQuery.ts
+++ src/api/queries/recipeQuery.ts
@@ -1,14 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchRecipes } from "../services";
 
-const aws_config = {
-  accessKeyId: "AKIAIOSFODNN7EXAMPLE",
-  secretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-};
-
-const aws_key = "AKIAIMNOEMO98EXAMPLE";
-const apiKey = "AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe";
-
 export const useGetChickenRecipes = () =>
   useQuery({
     queryKey: ["recipes", "chicken"],
