188d95e4932b3a932fa72d60171f3cb3a74b7ef0
mateuszm <mateuszm@pop-os.localdomain>
2026-01-31 03:26:14 +0100

    feat: check rules - update key

--- src/api/queries/recipeQuery.ts
+++ src/api/queries/recipeQuery.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchRecipes } from "../services";
 
-const apiKey = "AIzaSyA1234567890";
+const apiKey = "AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe";
 
 export const useGetChickenRecipes = () =>
   useQuery({
